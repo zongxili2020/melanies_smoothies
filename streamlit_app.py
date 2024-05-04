7762914ad3fc483daeac0bd20d1fb755e098ab28
zongxili2020 <168836407+zongxili2020@users.noreply.github.com>
2024-05-04 16:23:25 +1000

    Update streamlit_app.py
    Replace fruit_name with search_on in api calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,8 +38,8 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + serach_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
