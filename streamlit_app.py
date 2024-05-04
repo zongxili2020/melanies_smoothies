ed0c3f520bccf882a35b31572252862d91476a21
zongxili2020 <168836407+zongxili2020@users.noreply.github.com>
2024-05-04 16:17:18 +1000

    Update streamlit_app.py
    Fix to select columns

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.write("The name on your  Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe,use_container_width=True)
 
 # Convert the SnowPark Dataframe to a Pandas Dataframe so we can use the LOC function
